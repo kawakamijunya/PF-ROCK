dd11d0ebbb4bacbfdc6c88540a5ce81e209baf41
EC2 Default User <ec2-user@ip-172-31-45-76.ap-northeast-1.compute.internal>
2023-02-06 07:25:25 +0000

    users,postsコントローラー記述

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,13 +1,56 @@
 class Public::PostsController < ApplicationController
-  def new
-  end
+  before_action :authenticate_user! #ログインしているユーザーのみ
+  before_action :ensure_correct_user, only: [:update,:edit,:destroy] #update,edit,destroyは直打ち禁止
 
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
+    @post_comment = PostComment.new
   end
+  
+  def create 
+    @post = Post.new(post_params)
+    @post.user.id = current_user.id
+    if @post.save
+      redirect_to post_path(@post.id), notice: "投稿に成功しました。"
+    else
+      render 'new'
+  end 
 
   def edit
+    @post = Post.find(params[:id])
   end
+  
+  def update
+    @posts = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path_(@post), notice: "投稿の編集に成功しました。"
+    else
+      render 'edit'
+    end 
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title,:caption,:user_id)
+  end
+  
+  def ensure_correct_user
+    @post = Post.find(params[:id])
+    @user = @post.user
+    unless @user == current_user
+      redirect_to posts_path
+    end
+  end
+
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,10 +1,40 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+  before_action :ensure_correct_user, only: [:update, :edit, :destroy] #update,edit,destroyは直打ち禁止
+
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "プロフィールを更新しました。"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+
+
 end
