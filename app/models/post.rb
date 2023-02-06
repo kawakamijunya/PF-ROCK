e07e5fcb388228849df7ce4a5be25b12df66b342
EC2 Default User <ec2-user@ip-172-31-45-76.ap-northeast-1.compute.internal>
2023-02-06 06:23:19 +0000

    モデル記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,18 @@
 class Post < ApplicationRecord
-  
   belongs_to :user
   has_one_attached :image
-  
+  has_many :favorites, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  validates :title, presence:true
+  validates :caption, presence:true, length:{maximum:200}
+
   
   def get_image
     (image.attached?) ? image : 'no_image.jpg'
+  end
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,32 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :posts, dependent: :destroy
-         
+  has_many :favorites, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  #フォローした、されたの関係
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  #一覧画面で使う
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  
+  
+  #フォローした時の処理
+  def follow(user_id)
+    unless self == user_id
+      self.relationships.find_or_create_by(followed_id: user_id.to_i, follower_id: self.id)
+    end
+  end
+
+  #フォローを外す時の処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  #フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
+  
   
 end
