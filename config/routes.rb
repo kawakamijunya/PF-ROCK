ded1f2c7253dd1229fd3598024af8de2aadb4474
EC2 Default User <ec2-user@ip-172-31-45-76.ap-northeast-1.compute.internal>
2023-02-06 06:02:44 +0000

    モデル、コントローラー、ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,25 @@
 Rails.application.routes.draw do
-  #ユーザー用
+  root to: "homes#top"
+  
+  #利用者用
   devise_for :users,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+
+  namespace :public do
+    #投稿関連
+    resources :posts, only: [:new,:index,:show,:edit,:create,:destroy,:update] do
+      resource :favorites, only: [:create,:destroy]
+      resources :post_comments, only: [:create,:destroy]
+    end
+    #ユーザー関連
+    resources :users, only: [:index,:show,:edit,:update] do
+      resource :relationships, only: [:create,:destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
+    end
+  end
 
   #管理者用
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_05_074943) do
+ActiveRecord::Schema.define(version: 2023_02_06_054713) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -52,10 +52,32 @@ ActiveRecord::Schema.define(version: 2023_02_05_074943) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "caption"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
