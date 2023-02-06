e48d47022b15d7e720b7a32ee0108751a4668fb8
EC2 Default User <ec2-user@ip-172-31-45-76.ap-northeast-1.compute.internal>
2023-02-06 07:56:36 +0000

    いいねコメントコントローラー記述

---
+++ app/controllers/public/post_comments_controller.rb
@@ -0,0 +1,22 @@
+class Public::PostCommentsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = current_user.post_comments.new(post_comments_params)
+    @comment.post.id = @post.id
+    @comment.save
+  end
+  
+  def destroy
+    PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    redirect_to request.referer
+  end
+  
+  private
+  
+  def post_comments_params
+    params.require(:post_comment).permit(:comment)
+  end
+  
+end
