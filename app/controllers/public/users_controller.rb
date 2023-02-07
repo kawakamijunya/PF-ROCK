150ab80206afbb7b79751593ef4bfc8062ee71c5
EC2 Default User <radball0506@gmail.com>
2023-02-07 07:47:53 +0000

    ヘッダー調整

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -32,7 +32,7 @@ class Public::UsersController < ApplicationController
   def ensure_correct_user
     @user = User.find(params[:id])
     unless @user == current_user
-      redirect_to user_path(current_user)
+      redirect_to public_user_path(current_user)
     end
   end
 
