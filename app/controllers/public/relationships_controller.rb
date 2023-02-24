f2f6db6268ea422c12f5c5bd6b54eef91960594a
EC2 Default User <radball0506@gmail.com>
2023-02-24 04:55:16 +0000

    レイアウト調整

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -14,12 +14,12 @@ class Public::RelationshipsController < ApplicationController
   # フォロー一覧
   def followings
     user = User.find(params[:user_id])
-    @users = user.followings
+    @users = user.followings.order(:id).page(params[:page])
   end
   # フォロワー一覧
   def followers
     user = User.find(params[:user_id])
-    @users = user.followers
+    @users = user.followers.order(:id).page(params[:page])
   end
   
 end
